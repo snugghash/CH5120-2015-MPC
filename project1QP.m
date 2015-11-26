e8db4edc8345beb507fbf4a56aa22b9dbe55d826
snugghash <snugghash@gmail.com>
2015-11-26 23:31:17 +0530

    QP is as derived. Must verify.

--- project1QP.m
+++ project1QP.m
@@ -1,44 +1,43 @@
-% Model
-predictionHor = 11;
-controlHor = 7;
+%% Model, horizons, weights, initial states, reference trajectory.
+predictionHor = 11; %Prediction Horizon
+controlHor = 7; %Control Horizon
 diffHor = predictionHor - controlHor;
-load('linssmodel.mat');
+A = eye(5); %Placeholders, in case linssmodel.mat dosen't work out
+B = ones(5,2);
+load('linssmodel.mat'); %Get A,B
 C = eye(5);
 D = 0;
-[tmp,Xstates] = size(A);
-[tmp,Uinputs] = size(B);
+[~,Nstates] = size(A);
+[~,Ninputs] = size(B);
 Ysp = [-0.1 0.05 -0.4 0.6 -0.4]'; %For all k>0
-X0 = [0 0 0 0 0]';
+X0 = [0 0 0 0 0]'; %Starting states
+weightX = ones(Nstates,Nstates,predictionHor); %phiX, weighting matrix for X, until predictionHor
+weightU = ones(Ninputs*controlHor,Ninputs*controlHor); %phiU, weighting matrix for U
 
-% Computing H
-weightX = ones(Xstates,Xstates,predictionHor);
-weightU = ones(Uinputs,Uinputs,controlHor);
-%X = zeros(5, controlKnownTime+diffHor);
-%X(:,1) = A*X0;
-% DEBUG size(weightX(:,:,1))
-% DEBUG size((X(:,1)-Ysp)')
-% Computing the objective term from state L2 metric from set point
-% for i = 1:(controlKnownTime+diffHor)
-%     twoNorm = twoNorm + (X(:,i)-Ysp)'*weightX(:,:,i)*(X(:,i)-Ysp);
-%     X(:,i+1) = A*X(:,i) + B*U(:,i);
-% end
-% for i = 1:(controlKnownTime)
-%    twoNorm = twoNorm + U(:,i)'*weightU(:,:,i)*U(:,i);
-% end
-H_ = [];
-for i = 1:predictionHor
-    %Apowers = zeros(i+1,1)
-    % TODO Preallocate for Apowers
-    Apowers = [];
-    for j = 0:i-1
-        Apowers = [A^j*B Apowers];
+timeStart = 0;
+timeEnd = 400;
+
+%% Simple
+% Computing H,f
+for time = timeStart:timeEnd
+    ApowersB = [];
+    for j = 0:predictionHor-1
+        ApowersB = [A^j*B ApowersB];
     end
-    F = Apowers;
-    H_(:,:,i) = 2*(F'*weightX(:,:,i)*F + weightU(:,:,i));
-    f_(:,:,i) = -X0'*(A^i)'*weightX(:,:,i)*F + Ysp'*weightX(:,:,i)*F;
-    A_ = [];
-    b_ = [];
+    H = [];
+    for i = 1:predictionHor
+        F = ApowersB(:,end-i*Ninputs:end);
+        H = H + 2*(F'*weightX(:,:,i)*F);
+        
+        f(:,:,i) = -X0'*(A^i)'*weightX(:,:,i)*F + Ysp'*weightX(:,:,i)*F;
+        
+    end
+    H = H + 2*weightU;
+    % Constraints
+    constraintsA = [];
+    constraints_b = [];
+    [Uhat,obj,EXITFLAG,OUTPUT] = quadprog(H,f,constraintsA,constraints_b)
+    
 end
 
 
-[Uhat,obj,EXITFLAG,OUTPUT] = quadprog(H_,f_,A_,b_)
